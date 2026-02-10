415b4c35adcc2f59a6746242c74f331b03293267
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-10 20:38:20 +0530

    codeforces contest 2

--- 03.cpp
+++ 03.cpp
@@ -3,8 +3,45 @@ using namespace std;
 
 
 int main(){
+    int a;
+    cin >> a;
+    int arr[a];
     
+    for (int i = 0; i<a;i++){
+        cin >> arr[i];
+    }
     
+    int twocounter = 0;
+    int onecounter = 0;
+    int zercounter = 0;
+    
+    for (int i = 0; i<a; i++){
+        if (arr[i] == 0){
+            zercounter += 1;
+        }
+        else if (arr[i] == 1){
+            onecounter +=1;
+        }
+        else if(arr[i] == 2){
+            twocounter +=1;
+        }
+    }
+    
+    for (int i = 0; i< twocounter; i++){
+        cout << 2<< " ";
+    }
+    for (int i = 0; i< onecounter; i++){
+        cout << 1<< " ";
+    }
+    for (int i = 0; i< zercounter; i++){
+        if (i != zercounter -1){
+            cout << 0<< " ";
+        }
+        else{
+            cout << 0<< endl;
+        }
+        
+    }
     
     return 0;
 }
--- 04.cpp
+++ 04.cpp
@@ -1,10 +1,27 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    
+    if (a != 0 and b != 0){
+        if (a == b)
+            cout << "Yes\n";
+        else
+            cout << "No\n";
+    }
+    else if(a == 0 and b ==1){
+        cout << "Yes\n";
+    }
+    else if  (a== 1 and b == 0){
+        cout << "Yes\n";
+    }
+    else {
+        cout << "No"<< endl;
+    }
+    
 
-int main(){
-    
-    
-    
     return 0;
 }
