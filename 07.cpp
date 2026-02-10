72e48ba55fbd1a1d560a3cfac0bc19628242d155
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-10 21:49:46 +0530

    codeforces contest 2

---
+++ 07.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    int arr[a];
+    
+    for (int i = 0; i<a ; i++){
+        cin >> arr[i];
+    }
+    
+    
+    
+    return 0;
+}
