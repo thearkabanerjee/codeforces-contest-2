4ea44578ccd9488e81dfa4bc4be8d02fa2e86eeb
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-10 20:10:52 +0530

    codeforces contest 2

--- 01.cpp
+++ 01.cpp
@@ -3,8 +3,30 @@ using namespace std;
 
 
 int main(){
+    int a;
+    cin >> a;
+    int arr[a];
     
+    for (int i = 0; i<a;i++){
+        cin >> arr[i];
+    }
+    int passmarks;
+    cin >> passmarks;
     
+    int counterpass = 0;
+    int fail = 0;
+    
+    for (int i = 0; i<a; i++){
+        if (arr[i] >=passmarks){
+            counterpass +=1;
+        }
+        else {
+            fail += 1;
+        }
+    }
+    
+    cout << "Pass: "<< counterpass<< endl;
+    cout << "Fail: " <<fail<< endl;
     
     return 0;
 }
