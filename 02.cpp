cc8b97ebc4e0722edc6f76e2492e7f28b965139c
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-10 20:16:37 +0530

    codeforces contest 2

--- 02.cpp
+++ 02.cpp
@@ -3,8 +3,24 @@ using namespace std;
 
 
 int main(){
+    int a;
+    cin >> a;
+    int arr[a];
+    
+    for (int i = 0; i<a;i++){
+        cin >> arr[i];
+    }
+    
+    int min = arr[0];
+    int index = 1;
+    for (int i = 1; i <a ;i++){
+        if (arr[i]<= min){
+            min = arr[i];
+            index = i+1;
+        } 
+    }
     
     
-    
+    cout << index<< endl;
     return 0;
 }
